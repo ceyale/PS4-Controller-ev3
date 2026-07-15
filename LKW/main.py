9e60d96ce46683e008e1f95b661d81469a586f3b
ceyale <etienne.layec@gmail.com>
2026-07-15 14:21:25 +0200

    Add files via upload

---
+++ LKW/main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+import struct
+
+# Declare motors 
+left_speed_motor = Motor(Port.A)
+right_speed_motor = Motor(Port.B)
+steering_motor = Motor(Port.C)
+up_down_motor = Motor(Port.D)
+# Initialize variables. 
+# Assuming sticks are in the middle when starting.
+right_stick_x = 124
+right_stick_y = 124
+steering_motor.reset_angle(0)
+
+# A helper function for converting stick values (0 - 255)
+# to more usable numbers (-100 - 100)
+def scale(val, src, dst):
+    """
+    Scale the given value from the scale of src to the scale of dst.
+ 
+    val: float or int
+    src: tuple
+    dst: tuple
+ 
+    example: print(scale(99, (0.0, 99.0), (-1.0, +1.0)))
+    """
+    return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
+
+
+# Find the PS3 Gamepad:
+# /dev/input/event3 is the usual file handler for the gamepad.
+# look at contents of /proc/bus/input/devices if it doesn't work.
+infile_path = "/dev/input/event4"
+
+# open file in binary mode
+in_file = open(infile_path, "rb")
+
+# Read from the file
+# long int, long int, unsigned short, unsigned short, unsigned int
+FORMAT = 'llHHI'    
+EVENT_SIZE = struct.calcsize(FORMAT)
+event = in_file.read(EVENT_SIZE)
+
+while event:
+    (tv_sec, tv_usec, ev_type, code, value) = struct.unpack(FORMAT, event)
+    if ev_type == 3 and code == 3:
+        right_stick_x = value
+    if ev_type == 3 and code == 4:
+        right_stick_y = value
+
+    # Scale stick positions to -100,100
+    forward = scale(right_stick_y, (0,255), (100,-100))
+    left = scale(right_stick_x, (0,255), (100,-100))
+
+    # Set motor voltages. If we're steering left, the left motor
+    # must run backwards so it has a -left component
+    # It has a forward component for going forward too. 
+    if ev_type == 1 and code == 313:
+        if value == 1:          # gedrückt
+            left_speed_motor.dc(-100)
+            right_speed_motor.dc(-100)
+        elif value == 0:        # losgelassen
+            left_speed_motor.brake()
+            right_speed_motor.brake()
+
+    if ev_type == 1 and code == 312:
+        if value == 1:          # gedrückt
+            left_speed_motor.dc(100)
+            right_speed_motor.dc(100)
+        elif value == 0:        # losgelassen
+            left_speed_motor.brake()
+            right_speed_motor.brake()
+
+    # up down motor goes to specific place when specific buttons are pressed
+    if ev_type == 1 and code == 310:
+        if value == 1:          
+            up_down_motor.dc(40)
+    if ev_type == 1 and code == 311:
+        if value == 1:          
+            up_down_motor.dc(-40)
+    
+    if left > -20 and left < 20:
+        steering_motor.track_target(0)
+    else:
+        steering_motor.dc(int(left/3))
+    # Finally, read another event
+    event = in_file.read(EVENT_SIZE)
+
+in_file.close()
